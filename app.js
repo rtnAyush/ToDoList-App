618801f90814a10fc6ab2193aa7c90e9099be2c3
Ayush <7126924ak@gmail.com>
2023-01-28 08:21:15 +0530

    add variable port

--- app.js
+++ app.js
@@ -152,10 +152,10 @@ async function main() {
     }
 
   });
+ const port = process.env.PORT || 3000;
 
-
-  app.listen(3000, function() {
-    console.log("Server started on port 3000");
+  app.listen(port, function() {
+    console.log("Server started on port :"+ port);
   });
 };
 
